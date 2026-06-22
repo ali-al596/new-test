7f3361b64817a9a11683abc0ef96b029c1da04d0
Ali <alialmasi93113@gmail.com>
2026-06-22 12:17:55 +0330

    this is second update

--- test2.py
+++ test2.py
@@ -9,3 +9,23 @@ print("test")
 print("test")
 print("test")
 print("test")
+def get_extension(image_url):
+    extension: list[str] = ['.png','.png','.jpeg','.gif','.svg']
+    for exe in extension:
+        if exe in image_url:
+            return exe
+        # else:
+        #     raise ValueError("oops extension not find")
+
+
+def download_image(image_url:str, image_name:str,folder:str):
+    exe = get_extension(image_url)
+    if exe:
+        if folder:
+            name = f"{folder}/{image_name}{exe}"
+        else:
+            name = f"{image_name}{exe}"
+    else:
+        raise Exception("extension not found")
+    if os.path.isfile(name):
+        raise ValueError("this path already exist")
